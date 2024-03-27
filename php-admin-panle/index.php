866bd3b5d2cc47b994aff94bb3960e7cde1709b3
altaieb-mohammed <taiboxy@gmail.com>
2024-03-27 17:43:35 +0300

    create students-services

--- php-admin-panle/index.php
+++ php-admin-panle/index.php
@@ -8,7 +8,7 @@
      <div class="py-5">
           <div class="container">
                <?= alertmassage(); ?>
-               <h4>home bage</h4>
+               <h4>home bage hhhh</h4>
          </div>
      </div>
     
